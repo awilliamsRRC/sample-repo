b1af82d8b6e8566b1e7019a28c1f887ba3f6c289
rrc_F2024FA_awilliams <awilliams3@academic.rrc.ca>
2025-02-13 14:37:42 -0600

    test

---
+++ sample.js
@@ -0,0 +1,7 @@
+// This is a simple JavaScript program
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+let name = prompt("Enter your name:");
+alert(greet(name));
---
+++ sample.py
@@ -0,0 +1,6 @@
+# This is a simple Python program
+def greet(name):
+    return f"Hello, {name}!"
+
+name = input("Enter your name: ")
+print(greet(name))
